d27dd4421d25f77d42f87b77f0452422758e305e
sjrsjz <3244658766@qq.com>
2025-07-02 13:22:17 +0800

    更新版本号至0.1.3-2，调整提示词模板 修正提示词中的逻辑流名称以保持一致性

--- tests/test_template.py
+++ tests/test_template.py
@@ -4,7 +4,7 @@ from autogemini.template import parse_agent_output
 
 def test_parse_agent_output_basic():
     text = """
-<|start_header|>before_new_cycle<|end_header|>
+<|start_header|>think_before_new_cycle<|end_header|>
 This is the first block.
 <|start_header|>call_tool_code<|end_header|>
 print(default_api.some_tool())
@@ -15,7 +15,7 @@ print(default_api.some_tool())
 """
     blocks = parse_agent_output(text)
     assert len(blocks) == 4
-    assert blocks[0].type == "before_new_cycle"
+    assert blocks[0].type == "think_before_new_cycle"
     assert blocks[0].content == "This is the first block."
     assert blocks[1].type == "call_tool_code"
     assert blocks[1].content == "print(default_api.some_tool())"
